22e53da135984678973018da7c6dc5e373dafab9
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-03-14 13:05:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const cargarPeliculas = async() => {
 	// Si la respuesta es correcta
 	if(respuesta.status === 200){
 		const datos = await respuesta.json()
-		console.log(datos)
+
+		let peliculas = ''
+		datos.results.forEach(pelicula => {
+			peliculas += `
+			<div class="pelicula">
+			  <img class="poster"  src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+			  <h3 class="titulo">${pelicula.title}</h3>
+			</div>
+			`
+		})
+
+		document.getElementById('contenedor').innerHTML = peliculas
+
 	} else if(respuesta.status === 401){
 		console.log('Wrong key')
 	} else if(respuesta.status === 404){

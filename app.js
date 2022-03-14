f0d9e84198de0f7dea856f6dc158bb8c4614ba36
SebastianBuritica <93333842+SebastianBuritica@users.noreply.github.com>
2022-03-14 12:33:06 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const cargarPeliculas = async() => {
    try {		
-	const respuesta = await fetch('https://api.themoviedb.org/3/movie/550?api_key=2e76e7188abbdd2c393393fb6b0eee9a')
+	const respuesta = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=2e76e7188abbdd2c393393fb6b0eee9a')
 
-  console.log(respuesta)
+    console.log(respuesta)
 
 	// Si la respuesta es correcta
 	if(respuesta.status === 200){
 		const datos = await respuesta.json()
 		console.log(datos)
+	} else if(respuesta.status === 401){
+		console.log('Wrong key')
+	} else if(respuesta.status === 404){
+		console.log('The movie does not exist')
+	} else {
+		console.log('Unknown error')
 	}
 
  } catch(error){
